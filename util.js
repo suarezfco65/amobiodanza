af6548c8d46960997e49d03bebc5f7c1508d9cf9
suarezfco65 <56086678+suarezfco65@users.noreply.github.com>
2019-11-29 12:40:31 -0400

    Agregar la funcion includeHTML

--- util.js
+++ util.js
@@ -150,3 +150,34 @@ function flecha (x1, y1, size, direccion, angulo=0)	{
 	document.writeln('<line x1="'+x2+'" y1="'+y2+'" x2="'+x4+'" y2="'+y4+'"  stroke="gray" stroke-width="2"/>');
 	document.writeln('</g>');
 }
+/*
+Modo de uso:
+	<div w3-include-html="content.html"></div> 
+*/
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
