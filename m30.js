f67c6ee7a351e5c1f322a7bb4ea61518e9c1642f
suarezfco65 <56086678+suarezfco65@users.noreply.github.com>
2019-12-08 10:14:36 -0400

    PROGRAMA DE SEMINARIOS

---
+++ m30.js
@@ -0,0 +1,16 @@
+function m30()  {
+    var s='';
+    s += inicioNivel2('1','ESTRUCTURA Y EVOLUCIÓN DEL UNIVERSO')+``+finNivel2()+`
+`+inicioNivel2('2','TEORÍAS SOBRE EL ORIGEN DE LA VIDA')+``+finNivel2()+`
+`+inicioNivel2('3','DEL CAOS AL ORDEN. FRACTALES')+``+finNivel2()+`
+`+inicioNivel2('4','BIODANZA UNA NUEVA EPISTEMOLOGÍA')+``+finNivel2()+`
+`+inicioNivel2('5','EVOLUCIÓN DE LA IMAGEN DEL HOMBRE DESDE LA PREHISTORIA HASTA NUESTROS DÍAS')+``+finNivel2()+`
+`+inicioNivel2('6','TEMAS DE PSICOLOGÍA TEÓRICA PARA SER ESTUDIADOS')+``+finNivel2()+`
+`+inicioNivel2('7','ÉTICA')+``+finNivel2()+`
+`+inicioNivel2('8','REHABILITACIÓN EXISTENCIAL')+``+finNivel2()+`
+`+inicioNivel2('9','MOTIVACIONES PARA VIVIR')+``+finNivel2()+`
+`+inicioNivel2('10','LENGUAJE Y EXPRESIÓN')+``+finNivel2()+`
+`+inicioNivel2('11','ANTROPOLOGÍA DE LA DANZA')+``+finNivel2()+`
+`+finNivel2();
+return s;
+}
