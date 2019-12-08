2e34a93b9e1137843e14a4ef6fa0ab1deae87328
suarezfco65 <56086678+suarezfco65@users.noreply.github.com>
2019-12-08 10:13:05 -0400

    METODOLOGÍA V (EL GRUPO DE BIODANZA)

---
+++ m27.js
@@ -0,0 +1,36 @@
+function m27()  {
+    var s='';
+    s += inicioNivel2('1','EL GRUPO DE BIODANZA: MATRIZ DE RENACIMIENTOS')+`
+`+finNivel2()+`
+`+inicioNivel2('2','EL GRUPO DE BIODANZA')+`
+`+finNivel2()+`
+`+inicioNivel2('3','FUNCIONES DEL GRUPO DE BIODANZA')+`
+`+finNivel2()+`
+`+inicioNivel2('4','INTEGRACIÓN DEL GRUPO DE BIODANZA')+`
+`+finNivel2()+`
+`+inicioNivel2('5','PRIMEROS MOMENTOS')+`
+`+finNivel2()+`
+`+inicioNivel2('6','INTEGRACIÓN ORGÁNICA DE BASE AFETIVA')+`
+`+finNivel2()+`
+`+inicioNivel2('7','DINÁMICA DEL GRUPO DE BIODANZA')+`
+`+finNivel2()+`
+`+inicioNivel2('8','ACTUAR SOBRE LA PARTE SANA')+`
+`+finNivel2()+`
+`+inicioNivel2('9','SOBRE LA PARTICIPACIÓN EN EL CURSO SEMANAL Y LA SUPERACIÓN DE LOS MECANISMOS DE DEFENSA')+`
+`+finNivel2()+`
+`+inicioNivel2('10','FUNCIÓN DE LA PARTE TEÓRICA EN LA INTEGRACIÓN DEL GRUPO')+`
+`+finNivel2()+`
+`+inicioNivel2('11','RELATO DE VIVENCIAS')+`
+`+finNivel2()+`
+`+inicioNivel2('12','ENTREVISTA INDIVIDUAL')+`
+`+finNivel2()+`
+`+inicioNivel2('13','PARTICIPACIÓN DEL PROFESOR DE BIODANZA EN LAS SESIONES QUE DIRIGE')+`
+`+finNivel2()+`
+`+inicioNivel2('14','ACTITUD DEL PROFESOR DURANTE LAS SESIONES QUE DIRIGE')+`
+`+finNivel2()+`
+`+inicioNivel2('15','RELACIÓN ENTRE EL MICRO MUNDO GRUPAL Y EL MACRO MUNDO SOCIAL')+`
+`+finNivel2()+`
+`+inicioNivel2('16','ASPECTOS EXPERIMENTALES Y FENOMENOLÓGICOS DE BIODANZA')+`
+`+finNivel2();
+return s;
+}
