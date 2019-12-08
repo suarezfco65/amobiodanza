3a17272e13b86b1adee67c0dfd8c7da0a59feda2
suarezfco65 <56086678+suarezfco65@users.noreply.github.com>
2019-12-08 10:09:56 -0400

    'METODOLOGÍA I (SEMÁNTICA MUSICAL)

---
+++ m23.js
@@ -0,0 +1,16 @@
+function m23()  {
+    var s='';
+    s += inicioNivel2('1','MODELO TEÓRICO DE BIODANZA Y MÚSICA')+`
+`+finNivel2()+`
+`+inicioNivel2('2','HACIA UNA SEMÁNTICA MUSICAL')+`
+`+finNivel2()+`
+`+inicioNivel2('3','MÚSICA–MOVIMIENTO-VIVENCIA UNA ESTRUCTURA UNITARIA')+`
+`+finNivel2()+`
+`+inicioNivel2('4','CRITERIOS DE SELECCIÓN DE LAS MÚSICAS PARA BIODANZA')+`
+`+finNivel2()+`
+`+inicioNivel2('5','EJEMPLO ESQUEMÁTICO DE LA RELACIÓN ENTRE EMOCIÓN Y MÚSICA')+`
+`+finNivel2()+`
+`+inicioNivel2('6','EJEMPLOS DE EJERCICIOS Y MÚSICAS EN RELACIÓN AL PRECESO DE INTEGRACIÓN Y LAS LÍNEAS DE VIVENCIA')+`
+`+finNivel2();
+return s;
+}
