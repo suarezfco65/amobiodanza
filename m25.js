761a90b8329b8a7c4ede7fab24e3a02a5db6938c
suarezfco65 <56086678+suarezfco65@users.noreply.github.com>
2019-12-08 10:12:02 -0400

    METODOLOGÍA III (LA SESIÓN DE BIODANZA II)

---
+++ m25.js
@@ -0,0 +1,18 @@
+function m25()  {
+    var s='';
+    s += inicioNivel2('1','SESIONES DE INICIACIÓN');+`
+`+finNivel2();+`
+`+inicioNivel2('2','EJEMPLOS DE ESTRUCTURAS DE VIVENCIAS DE INICIACIÓN');+`
+`+finNivel2();+`
+`+inicioNivel2('3','SESIONES DE PROFUNDIZACIÓN Y DE RADICALIZACIÓN DE VIVENCIAS');+`
+`+finNivel2();+`
+`+inicioNivel2('4','INTENSIDAD DE LA VIVENCIA');+`
+`+finNivel2();+`
+`+inicioNivel2('5','EJEMPLOS DE ESTRUCTURAS DE VIVENCIA DE PROFUNDIZACIÓN Y RADICALIZACIÓN EN LAS CINCO LÍNEAS');+`
+`+finNivel2();+`
+`+inicioNivel2('6','DIFICULTADES Y ERRORES METODOLÓGICOS');+`
+`+finNivel2();+`
+`+inicioNivel2('7','CÓDIGO PARA ALCANZAR UNA LUCIDEZ DISCRIMINATIVA QUE PERMITA DIFERENCIAR EL SISTEMA BIODANZA DE OTRAS PROPUESTAS DE DESARROLLO HUMANO');+`
+`+finNivel2();
+return s;
+}
