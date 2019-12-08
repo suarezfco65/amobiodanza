499e3e3ee343999b188074fb9b861de4d999d88b
suarezfco65 <56086678+suarezfco65@users.noreply.github.com>
2019-12-08 10:11:30 -0400

    METODOLOGÍA II (LA SESIÓN DE BIODANZA)

---
+++ m24.js
@@ -0,0 +1,34 @@
+function m07()  {
+    var s='<H4>CAPÍTULO I: LA SESIÓN DE BIODANZA</H4>';
+    s += inicioNivel2('I-1','SIGNIFICADO ANTROPOLÓGICO')+`
+`+finNivel2()+`
+`+inicioNivel2('I-2','OBJETIVO')+`
+`+finNivel2()+`
+`+inicioNivel2('I-3','NIVELES')+`
+`+finNivel2()+`
+`+inicioNivel2('I-4','DEMOSTRACIONES PÚBLICAS Y AULAS ABIERTAS')+`
+`+finNivel2()+`
+`+inicioNivel2('I-5','DURACIÓN')+`
+`+finNivel2()+`
+`+inicioNivel2('I-6','ESTRUCTURA')+`
+`+finNivel2()+`
+<H4>CAPÍTULO II: LOS EJERCICIOS DE BIODANZA</H4>
+`+inicioNivel2('II-1','EFECTOS FISIOLOGIOS')+`
+`+finNivel2()+`
+`+inicioNivel2('II-2','EJERCICIOS BÁSICOS Y EJERCICIOS ESPECÍFICOS')+`
+`+finNivel2()+`
+`+inicioNivel2('II-3','CLASIFICACIÓN')+`
+`+finNivel2()+`
+`+inicioNivel2('II-4','LA MÚSICA')+`
+`+finNivel2()+`
+<H4>CAPÍTULO III: ESTRUCTURACIÓN DE LA PARTE VIVENCIAL</H4>
+`+inicioNivel2('III-1','EJEMPLOS DE EJERCICIOS DE INTEGRACIÓN, COMUNICACIÓN AFECTIVA Y COMUNICACIÓN, Y DÓNDE COLOCARLOS EN LA ESTRUCTURA DE LA VIVENCIA')+`
+`+finNivel2()+`
+`+inicioNivel2('III-2','EJEMPLOS DE EJERCICIOS ESPECÍFICOS DE EXPRESIÓN DE LOS POTENCIALES GENÉTICOS')+`
+`+finNivel2()+`
+`+inicioNivel2('III-3','EL PASAJE DE UN EJERCICIO A OTRO')+`
+`+finNivel2()+`
+`+inicioNivel2('III-4','ACTIVACIÓN FINAL')+`
+`+finNivel2();
+return s;
+}
