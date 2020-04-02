bcece097afe96846498fde23caa95e7116dedb3c
suarezfco65 <56086678+suarezfco65@users.noreply.github.com>
2020-04-02 15:30:49 -0400

    SEXUALIDAD
    Adicionando el relato

--- m13.js
+++ m13.js
@@ -654,6 +654,10 @@ más profundos y graves:
 <p>Les pedimos que traigan los alimentos lo mas preparados posibles.</p>
 <p>La Inversión del Modelo sera 6 dolares, cancelados al equivalente en BsS segun paridad enviada el jueves de la semana del Modulo,   y pernocta 3.000bs noche, favor cancelar por adelantado. Agradecemos puntualidad .</p>
 <p>Los abrazo apretadito. Florelena
+`+finNivel2()
++inicioNivel2('OP-RL','RELATO FRANCISCO SUÁREZ')+`
+<p>A través de esta línea de vivencia, los elementos, como el placer, la genialidad, la sensualidad, el erotismo, y la divinidad; en la sexualidad, son todos una manifestación de la fuerza vital; que además se distribuye en todo el cuerpo como fuente de placer, y no es tema exclusivo de los genitales. Sentir también que la sexualidad se encuentra en el impulso, las ganas, el deseo de realizar cada una de las actividades cotidianas (bañarse, comer, dormir, contacto con otras personas, caminar, etc); también el sentir estímulos en diferentes objetos</p>
+<p>Interesante el tema sobre pareja ecológica, en esa forma de vida vinculada, don está presente la individualidad, la expresión de sus potencialidades, la presencia del compañero.</p>
 `+finNivel2();
 return s;
 }
