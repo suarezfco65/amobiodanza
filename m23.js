0cfa8c548b2e5e47cc4bec9ab97dc82c1c2a4e80
suarezfco65 <56086678+suarezfco65@users.noreply.github.com>
2020-07-22 17:42:15 -0400

    Update m23.js

--- m23.js
+++ m23.js
@@ -2,24 +2,24 @@ function m23()  {
     var s='';
     s += inicioNivel2('1','MODELO TEÓRICO DE BIODANZA Y MÚSICA')+`
 <ol>
-<li>Semenjanzas entre el modelo teórico de Biodanza y la estructura de una cantata.<li>
-<li>Identidad y música.<li>
-<li>La vivencia oceánica<li>
+<li>Semenjanzas entre el modelo teórico de Biodanza y la estructura de una cantata.</li>
+<li>Identidad y música.</li>
+<li>La vivencia oceánica</li>
 <li>Prolepsis musical y desarrollo.
-<p>En los primeros compases de una música está, por decirlo así, el embrión de la totalidad. La música, tal como la requiere Biodanza -como una forma de energía capaz de estimular el despertar de potenciales biológicos y emocionales- tiene como primera exigencia un sentido de unidad y un desarrollo coherente. Si no cumple con estas condiciones, puede poseer valores sonoros interesantes, pero no puede ser utilizada, por apartarse de la fuente biológica. Así, una música inicia en los primeros compases la activación de un patrón de respuesta afectivo-motor-expresivo que se despliega con toda su fuerza a través de la danza, debido a la prolepsis musical que adelanta, por así decirlo, las propuestas psicomotoras en el tiempo espacio. La prolepsis musical se integra a la prolepsis afectiva en un solo conjunto de acción y emoción</p><li>
+<p>En los primeros compases de una música está, por decirlo así, el embrión de la totalidad. La música, tal como la requiere Biodanza -como una forma de energía capaz de estimular el despertar de potenciales biológicos y emocionales- tiene como primera exigencia un sentido de unidad y un desarrollo coherente. Si no cumple con estas condiciones, puede poseer valores sonoros interesantes, pero no puede ser utilizada, por apartarse de la fuente biológica. Así, una música inicia en los primeros compases la activación de un patrón de respuesta afectivo-motor-expresivo que se despliega con toda su fuerza a través de la danza, debido a la prolepsis musical que adelanta, por así decirlo, las propuestas psicomotoras en el tiempo espacio. La prolepsis musical se integra a la prolepsis afectiva en un solo conjunto de acción y emoción</p></li>
 <li>Música de los orígenes.<br>
 Armonía de las esferas.<br>
 La naturaleza, maestra de música.<br>
 Ritmos biológicos.<br>
-El niño y la música.<li>
+El niño y la música.</li>
 <li>Neurología de la función musical.<br>
 Hemisferio cerebral de la música.<br>
-Sistemas sincrónico y diacrónico.<li>
+Sistemas sincrónico y diacrónico.</li>
 <li>Inteligencia musical.<br>
 Estética y poyética musical.<br>
 Selectividad musical.<br>
 Poesía y música.<br>
-Mito y música.<li>
+Mito y música.</li>
 <li>El canto.<br> 
 Qué es una canción.<br> 
 Canto primal.<br>
@@ -28,15 +28,15 @@ Canto para sí mismo y para el otro.<br>
 Biología del coro.<br>
 El cantar de los cantares: canto de amor.<br>
 <br>
-<sup>∗</sup><sub><i>A pesar de faltar el contenido de algunos subtítulos, creo valioso presentarlos porque representan el esquema que utilizo habitualmente en el ámbito de este tema.</i></sub><br><li>
+<sup>∗</sup><sub><i>A pesar de faltar el contenido de algunos subtítulos, creo valioso presentarlos porque representan el esquema que utilizo habitualmente en el ámbito de este tema.</i></sub><br></li>
 <li>El sonido de las cosas.<br>
-Lenguaje de la naturaleza.<li>
+Lenguaje de la naturaleza.</li>
 <li>Sociología de la música.<br> 
 Sincronización rítmica con otro.<br>
-Armonía con otro.<li>
-<li>La música creativa.<li>
+Armonía con otro.</li>
+<li>La música creativa.</li>
 <li>Creación musical y danza creativa.<br>
-Niveles de experiencia musical.<li>
+Niveles de experiencia musical.</li>
 </ol>
 `+finNivel2()+`
 `+inicioNivel2('2','HACIA UNA SEMÁNTICA MUSICAL')+`
