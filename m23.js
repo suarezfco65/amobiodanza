5194d4ee2b0c38320ce177860ab22e8cdfdf27de
suarezfco65 <56086678+suarezfco65@users.noreply.github.com>
2020-07-22 17:37:49 -0400

    METODOLOGÍA I – SEMÁNTICA MUSICAL

--- m23.js
+++ m23.js
@@ -1,6 +1,43 @@
 function m23()  {
     var s='';
     s += inicioNivel2('1','MODELO TEÓRICO DE BIODANZA Y MÚSICA')+`
+<op>
+<li>Semenjanzas entre el modelo teórico de Biodanza y la estructura de una cantata.<li>
+<li>Identidad y música.<li>
+<li>La vivencia oceánica<li>
+<li>Prolepsis musical y desarrollo.
+<p>En los primeros compases de una música está, por decirlo así, el embrión de la totalidad. La música, tal como la requiere Biodanza -como una forma de energía capaz de estimular el despertar de potenciales biológicos y emocionales- tiene como primera exigencia un sentido de unidad y un desarrollo coherente. Si no cumple con estas condiciones, puede poseer valores sonoros interesantes, pero no puede ser utilizada, por apartarse de la fuente biológica. Así, una música inicia en los primeros compases la activación de un patrón de respuesta afectivo-motor-expresivo que se despliega con toda su fuerza a través de la danza, debido a la prolepsis musical que adelanta, por así decirlo, las propuestas psicomotoras en el tiempo espacio. La prolepsis musical se integra a la prolepsis afectiva en un solo conjunto de acción y emoción</p><li>
+<li>Música de los orígenes.<br>
+Armonía de las esferas.<br>
+La naturaleza, maestra de música.<br>
+Ritmos biológicos.<br>
+El niño y la música.<li>
+<li>Neurología de la función musical.<br>
+Hemisferio cerebral de la música.<br>
+Sistemas sincrónico y diacrónico.<li>
+<li>Inteligencia musical.<br>
+Estética y poyética musical.<br>
+Selectividad musical.<br>
+Poesía y música.<br>
+Mito y música.<li>
+<li>El canto.<br> 
+Qué es una canción.<br> 
+Canto primal.<br>
+Canto y trance.<br>
+Canto para sí mismo y para el otro.<br>
+Biología del coro.<br>
+El cantar de los cantares: canto de amor.<br>
+<br>
+<sup>∗</sup><sub><i>A pesar de faltar el contenido de algunos subtítulos, creo valioso presentarlos porque representan el esquema que utilizo habitualmente en el ámbito de este tema.</i></sub><br><li>
+<li>El sonido de las cosas.<br>
+Lenguaje de la naturaleza.<li>
+<li>Sociología de la música.<br> 
+Sincronización rítmica con otro.<br>
+Armonía con otro.<li>
+<li>La música creativa.<li>
+<li>Creación musical y danza creativa.<br>
+Niveles de experiencia musical.<li>
+</op>
 `+finNivel2()+`
 `+inicioNivel2('2','HACIA UNA SEMÁNTICA MUSICAL')+`
 `+finNivel2()+`
@@ -11,6 +48,8 @@ function m23()  {
 `+inicioNivel2('5','EJEMPLO ESQUEMÁTICO DE LA RELACIÓN ENTRE EMOCIÓN Y MÚSICA')+`
 `+finNivel2()+`
 `+inicioNivel2('6','EJEMPLOS DE EJERCICIOS Y MÚSICAS EN RELACIÓN AL PRECESO DE INTEGRACIÓN Y LAS LÍNEAS DE VIVENCIA')+`
+`+finNivel2()+`
+`+inicioNivel2('7','ANEXO: CRITERIOS PARA LA SELECCIÓN DE MÚSICA EN LAS CINCO LÍNEAS DE VIVENCIAS')+`
 `+finNivel2();
 return s;
 }
