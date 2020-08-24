5c78532e29250d6ea1411fb339164ae37685a4c4
suarezfco65 <56086678+suarezfco65@users.noreply.github.com>
2020-08-24 10:21:46 -0400

    METODOLOGÍA II – LA SESIÓN DE BIODANZA

--- m24.js
+++ m24.js
@@ -1,16 +1,68 @@
 function m24()  {
     var s='<H4>CAPÍTULO I: LA SESIÓN DE BIODANZA</H4>';
+    s += `
+<p>La sesión de Biodanza es una ceremonia de transformación interna mediante la vivencia. Consiste en un proceso de integración de la identidad en sus aspectos viscerales y comportamentales. Como ceremonia, requiere cumplir con ciertas exigencias que le den estructura. No puede, por lo tanto, confundirse con una reunión danzante de carácter público y la privacidad es una condición esencial e indispensable para su realización.</p>
+<p>La sesión de Biodanza es una fuente de vivencias y no un espectáculo, excepto cuando premeditadamente se crean coreografías en la línea de creatividad. Las sesiones tienen, por momentos, pasajes expresivos y eróticos de profunda intimidad, que se generan dentro del contexto de ceremonia sagrada en el sentido de sinceridad absoluta del encuentro. Dentro de este contexto trascendente, la sesión de Biodanza genera una libertad interior que jamás se alcanza en la vida pública.</p>
+<p>El lugar donde se realiza la sesión debe tener condiciones de dignidad y decoro propias de un ‘centro del mundo’, en el sentido junguiano, donde se inducen transformaciones internas. Debe poseer, además, las condiciones básicas de privacidad donde los participantes puedan tener encuentros auténticos, sin espectadores. No se puede, por lo tanto, realizar una sesión en un lugar público, en un bar o en otro recinto frecuentado por el público.</p>
+<p>El espacio privado de Biodanza, formado por el grupo que tiene ya la iniciación teórica y ritual, genera situaciones de liberación que para la mirada de un extraño puede resultar chocantes. La presencia de extraños es muy perjudicial porque no perciben el misterio de la vivencia y transmiten ulteriormente una imagen distorsionada. Es necesario, por lo tanto, preservar la belleza interior y la privacidad de la sesión de Biodanza, para que sea una auténtica ceremonia de transformación interna donde sea posible la veracidad del encuentro.</p>
+`;
     s += inicioNivel2('I-1','SIGNIFICADO ANTROPOLÓGICO')+`
+<p>Considerando los antecedentes míticos en los que se inspira, la sesión de Biodanza es una ceremonia de celebración de la vida. Los participantes tienen que ser ‘iniciados’ dentro de un grupo estructurado y no se deben aceptar invitados ocasionales para contemplar la sesión.</p>
+<p>El cribo (cedazo) es un símbolo de iniciación y predestinación porque separa el grano de la paja, la vida de la muerte. En las ceremonias eleusianas de celebración de la vida, la ‘iniciación’ era un acto fundamental que permitía el acceso a los Misterios de Eleusis.</p>
+<center><img src="misteriosDeEulises.png"></center>
+<p>Las ceremonias de ‘recibir el contacto’ y el bautismo, practicado por Juan el Bautista, tienen el mismo sentido.</p>
+<p>La ‘iniciación’ en Biodanza se lleva a efecto en las sesiones para iniciantes, en las que se dan las claves para comprender lo que es la ‘danza de la vida’ y se realizan las primeras vivencias.</p>
 `+finNivel2()+`
-`+inicioNivel2('I-2','OBJETIVO')+`
+`+inicioNivel2('I-2','OBJETIVO DE LA SESIÓN DE BIODANZA')+`
+<p>El objetivo de la sesión de Biodanza es el mismo de Biodanza y consiste en la integración de la identidad mediante la expresión de los potenciales genéticos estimulados en las cinco líneas de vivencia. El concepto de integración de la identidad se resume en la posibilidad de integrar el propio comportamiento con la expresión de uno o más potenciales genéticos. La expresión de estos potenciales puede manifestarse plenamente en el comportamiento y en el proyecto de vida. Cuando se manifiesta sólo parcialmente, o cuando no se manifiesta debido a la acción represiva de los ecofactores negativos, la integración es insuficiente.</p>
 `+finNivel2()+`
 `+inicioNivel2('I-3','NIVELES')+`
+<p>Las sesiones de Biodanza pueden ser de tres niveles:
+<ol>
+<li>Iniciación.</li>
+<li>Profundización.</li>
+<li>Radicalización de Vivencias.</li>
+</ol></p>
+<p>El objetivo de las sesiones de Iniciación es iniciar el proceso de integración. Estas sesiones son dirigidas a personas que tienen poco o ninguna experiencia en Biodanza.</p>
+<p>Las sesiones de Profundización tienen el objetivo de elevar el grado de intensidad vivencial en las cinco líneas: vitalidad, sexualidad, creatividad, afectividad y trascendencia.</p>
+<p>Las sesiones de Radicalización de Vivencias tienen por objetivo estabilizar cambios profundos ya obtenidos.</p>
 `+finNivel2()+`
 `+inicioNivel2('I-4','DEMOSTRACIONES PÚBLICAS Y AULAS ABIERTAS')+`
+<p>Cuando se realiza una ‘aula abierta’ o ‘aula de presentación’ de Biodanza -con el objetivo de darla a conocer- o una ‘sesión televisada’, es indispensable comprender que se trata de ‘mostrar’ algunos ejercicios de ritmo y armonía de carácter estético y una concepción nueva de la vida, más creativa y afectiva.</p>
+<p>Las ‘sesiones abiertas’ deben comunicar el sentido de la ‘alegría de vivir’ y el poder transformador de Biodanza, sin generar aprehensiones.</p>
 `+finNivel2()+`
-`+inicioNivel2('I-5','DURACIÓN')+`
+`+inicioNivel2('I-5','DURACIÓN DE LA SESIÓN DE BIODANZA')+`
+<p>La duración de una sesión de Biodanza, comprendiendo sus dos partes (teórica y vivencial), es de dos horas.</p>
+<p>En los Cursos Semanales de Profundización y Radicalización de Vivencias se puede llegar a una duración máxima de dos horas y media.</p>
+<p>En una maratón se puede llegar a un tiempo máximo de tres horas.</p>
+<p>Es importante saber que en una hora y media de práctica se alcanza a inducir vivencias profundas.</p>
+<p>En una ‘aula abierta’, la cual tiene por objetivo dar a conocer Biodanza, la duración media total (teoría y práctica) puede ser de una hora y media.</p>
 `+finNivel2()+`
-`+inicioNivel2('I-6','ESTRUCTURA')+`
+`+inicioNivel2('I-6','ESTRUCTURA DE LA SESIÓN DE BIODANZA')+`
+<p>La sesión de Biodanza se constituye de dos partes: teórica y vivencial.</p>
+<h4>Parte Teórica:</h4>
+<p>Al comenzar la sesión de Biodanza, se presenta una breve explicación teórica que tiene por objeto crear la motivación para danzar y poner al Sistema Biodanza dentro de un contexto teórico y filosófico amplio y profundo.</p>
+<p>El abordaje teórico debe enfatizar el hecho de que Biodanza no es solamente un conjunto de ejercicios con música o un sistema convencional de expresión de emociones, sino una nueva visión del mundo, un proceso de mudanza y desarrollo de las potencialidades humanas. Se trata de aprender a ‘danzar la vida’ y descubrir el placer de vivir.</p>
+<p>Temas teóricos importantes pueden ser los siguientes:
+<ul>
+<li>Definición de Biodanza.</li>
+<li>Prioridad de la vivencia sobre el pensamiento y la palabra.</li>
+<li>Modelo teórico de Biodanza.</li>
+<li>Líneas de vivencia.</li>
+<li>Principio biocéntrico.</li>
+<li>Mecanismos de acción de Biodanza.</li>
+<li>Inconsciente vital.</li>
+<li>Conexión con la vida.</li></ul></p>
+<p>Los temas teóricos deben ser desarrollados progresivamente en las clases semanales, reservando un espacio de tiempo, en cada sesión, para responder a las preguntas de los alumnos. La parte teórica debe realizarse siempre al inicio de la sesión y debe durar al máximo treinta minutos. El tiempo restante debe ser dedicado a la vivencia.</p>
+<h4>Parte Práctica o Vivencia:</h4>
+<p>La parte práctica de la sesión de Biodanza es también llamada vivencia y consiste en una secuencia de ejercicios de Biodanza que deben realizarse después de la parte teórica. Cada ejercicio induce una vivencia singular. La parte práctica o ‘vivencia’ es una secuencia de diferentes y breves vivencias singulares, deflagradas por los ejercicios y sus respectivas músicas.</p>
+<h4>Estructura de la Vivencia de Biodanza (parte práctica de la sesión):</h4>
+<p>La vivencia debe ser estructurada según el Modelo Teórico de Biodanza. Durante la vivencia hay una continua pulsación entre dos polos, representados en el eje horizontal del Modelo Teórico: conciencia de la propia identidad y regresión.</p>
+
+
+
+
+
 `+finNivel2()+`
 <H4>CAPÍTULO II: LOS EJERCICIOS DE BIODANZA</H4>
 `+inicioNivel2('II-1','EFECTOS FISIOLOGIOS')+`
