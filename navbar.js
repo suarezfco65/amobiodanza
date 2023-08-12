eb6443f86dcf533cfc95ea902d659dcebacf0f61
Francisco Suarez <56086678+suarezfco65@users.noreply.github.com>
2023-08-12 05:32:10 -0400

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -259,3 +259,12 @@ js.id = 'ps';
 $( document ).ready(function() {
    document.head.appendChild(js);
 });
+async function sha256(message) {
+const msgBuffer = new TextEncoder().encode(message); // Codifica la cadena a utf-8
+const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer); // Genera el hash SHA-256 del buffer de mensajes
+
+const hashArray = Array.from(new Uint8Array(hashBuffer)); // Convierte el hash buffer en un array de bytes
+const hashHex = hashArray
+.map((b) => b.toString(16).padStart(2, "0"))
+.join(""); // Convierte el array de bytes en una cadena hexadecimal return hashHex;
+}
